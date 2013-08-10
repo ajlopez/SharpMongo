a6b6f662a73a0ee2631da77c103806681f6fe334
ajlopez <webmaster@ajlopez.com>
2013-08-10 20:31:44 -0300

    Collection find one document test

--- Src/SharpMongo.Core.Tests/CollectionTests.cs
+++ Src/SharpMongo.Core.Tests/CollectionTests.cs
@@ -20,5 +20,22 @@
             Assert.IsNotNull(document.GetMember("Id"));
             Assert.IsInstanceOfType(document.GetMember("Id"), typeof(Guid));
         }
+
+        [TestMethod]
+        public void FindOneDocument()
+        {
+            Collection collection = new Collection();
+            DynamicDocument document = new DynamicDocument("Name", "Adam", "Age", 800);
+
+            collection.Insert(document);
+
+            var result = collection.Find();
+
+            Assert.AreEqual(1, result.Count());
+
+            Assert.AreEqual(document.GetMember("Id"), result.First().GetMember("Id"));
+            Assert.AreEqual("Adam", result.First().GetMember("Name"));
+            Assert.AreEqual(800, result.First().GetMember("Age"));
+        }
     }
 }
--- Src/SharpMongo.Core/Collection.cs
+++ Src/SharpMongo.Core/Collection.cs
@@ -7,9 +7,19 @@
 
     public class Collection
     {
+        private IList<DynamicDocument> documents = new List<DynamicDocument>();
+
         public void Insert(DynamicDocument document)
         {
             document.SetMember("Id", Guid.NewGuid());
+            this.documents.Add(document);
+        }
+
+        public IEnumerable<DynamicDocument> Find()
+        {
+            foreach (var document in this.documents)
+                yield return document;
         }
     }
 }
+
