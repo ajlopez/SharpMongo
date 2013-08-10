867462e6cd1bac613f2e51d81a4f3d5724b4b39f
ajlopez <webmaster@ajlopez.com>
2013-08-10 20:18:21 -0300

    Dynamic Object first tests

---
+++ Src/SharpMongo.Core.Tests/DynamicDocumentTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SharpMongo.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DynamicDocumentTests
+    {
+        [TestMethod]
+        public void CreateAndGetUndefinedMember()
+        {
+            DynamicDocument document = new DynamicDocument();
+
+            Assert.IsNull(document.GetMember("Name"));
+        }
+
+        [TestMethod]
+        public void GetDefinedMember()
+        {
+            DynamicDocument document = new DynamicDocument("Name", "Adam");
+
+            Assert.AreEqual("Adam", document.GetMember("Name"));
+        }
+    }
+}
---
+++ Src/SharpMongo.Core/DynamicDocument.cs
@@ -0,0 +1,26 @@
+﻿namespace SharpMongo.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DynamicDocument
+    {
+        private IDictionary<string, object> values = new Dictionary<string, object>();
+
+        public DynamicDocument(params object[] arguments)
+        {
+            for (int k = 0; k < arguments.Length; k += 2)
+                values[arguments[k].ToString()] = arguments[k + 1];
+        }
+
+        public object GetMember(string name)
+        {
+            if (values.ContainsKey(name))
+                return values[name];
+
+            return null;
+        }
+    }
+}
