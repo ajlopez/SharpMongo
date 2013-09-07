820999c0dadc64824e36c77f694f6736b497d20c
ajlopez <webmaster@ajlopez.com>
2013-09-07 19:20:09 -0300

    Engine, with first tests

---
+++ Src/SharpMongo.Core.Tests/EngineTests.cs
@@ -0,0 +1,64 @@
+﻿namespace SharpMongo.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EngineTests
+    {
+        [TestMethod]
+        public void GetUnknownDocumentBase()
+        {
+            Engine engine = new Engine();
+
+            Assert.IsNull(engine.GetDocumentBase("Unknown"));
+        }
+
+        [TestMethod]
+        public void CreateDocumentBase()
+        {
+            Engine engine = new Engine();
+
+            var result = engine.CreateDocumentBase("Genesis");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Genesis", result.Name);
+        }
+
+        [TestMethod]
+        public void CreateDocumentBaseTwice()
+        {
+            Engine engine = new Engine();
+
+            engine.CreateDocumentBase("Genesis");
+
+            try
+            {
+                engine.CreateDocumentBase("Genesis");
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+                Assert.AreEqual("Document Base 'Genesis' already exists", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void GetDocumentBase()
+        {
+            Engine engine = new Engine();
+
+            var dbase = engine.CreateDocumentBase("Genesis");
+
+            var result = engine.GetDocumentBase("Genesis");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Genesis", result.Name);
+            Assert.AreSame(dbase, result);
+        }
+    }
+}
---
+++ Src/SharpMongo.Core/Engine.cs
@@ -0,0 +1,31 @@
+﻿namespace SharpMongo.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Engine
+    {
+        IDictionary<string, DocumentBase> dbases = new Dictionary<string, DocumentBase>();
+
+        public DocumentBase GetDocumentBase(string name)
+        {
+            if (this.dbases.ContainsKey(name))
+                return this.dbases[name];
+
+            return null;
+        }
+
+        public DocumentBase CreateDocumentBase(string name)
+        {
+            if (this.dbases.ContainsKey(name))
+                throw new InvalidOperationException(string.Format("Document Base '{0}' already exists", name));
+
+            DocumentBase dbase = new DocumentBase(name);
+            this.dbases[name] = dbase;
+
+            return dbase;
+        }
+    }
+}
