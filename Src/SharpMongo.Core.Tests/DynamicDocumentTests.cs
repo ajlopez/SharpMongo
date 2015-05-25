d4e8ffd99fe8d048637d3b14a2988fc9cf09a3ef
ajlopez <webmaster@ajlopez.com>
2015-05-25 07:10:52 -0300

    Document Project with new field expression

--- Src/SharpMongo.Core.Tests/DynamicDocumentTests.cs
+++ Src/SharpMongo.Core.Tests/DynamicDocumentTests.cs
@@ -61,6 +61,24 @@
         }
 
         [TestMethod]
+        public void ProjectNewFieldWithExpression()
+        {
+            DynamicDocument document = new DynamicDocument("Name", "Adam", "Age", 800);
+            DynamicDocument projection = new DynamicDocument("Has800", new DynamicDocument("$eq", new object[] { "$Age", 800 }));
+
+            var result = document.Project(projection);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.GetMember("Name"));
+            Assert.AreEqual("Adam", result.GetMember("Name"));
+            Assert.IsNotNull(result.GetMember("Age"));
+            Assert.AreEqual(800, result.GetMember("Age"));
+            Assert.IsNotNull(result.GetMember("Has800"));
+            Assert.AreEqual(true, result.GetMember("Has800"));
+            Assert.AreEqual(3, result.GetMemberNames().Count());
+        }
+
+        [TestMethod]
         public void ProjectExcludingAge()
         {
             DynamicDocument document = new DynamicDocument("Name", "Adam", "Age", 800);
